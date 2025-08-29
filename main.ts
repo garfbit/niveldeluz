ff54ef9b35c25f2af572b02a4426db7e343520e8
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-28 23:52:05 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.forever(function () {
+    basic.showNumber(input.lightLevel())
+    if (input.lightLevel() < 50) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
